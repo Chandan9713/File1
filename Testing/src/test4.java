b83b411a0ec2a97b362a655c49ecfe830d98c9e4
CHANDAN <sss@sss.com>
2018-05-19 14:18:23 +0530

    java file added

---
+++ Testing/src/test4.java
@@ -0,0 +1,21 @@
+
+public class test4 {
+	
+	public static void main(String[] args)
+	{
+		
+     int a =1;
+     int b =2;
+     int c= a+b;
+     if( a == b)
+     {
+    	 System.out.println("A is equal to b");
+     }
+     else if (a <= b)
+     {
+    	 System.out.println("b is greater than a");
+     }
+	 System.out.println("this is testing project" +c);
+	}
+
+}
